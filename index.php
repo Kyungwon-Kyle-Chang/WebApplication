718d7a5438ddcf0891c9579a3b005744b36c6700
Kyungwon Chang <kwmnusa@naver.com>
2017-08-17 09:53:01 +0900

    bootstrap 기초
    bootstrap 연습

--- index.php
+++ index.php
@@ -8,39 +8,64 @@
 <html>
 <head>
 	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+  <!-- Bootstrap -->
+   <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <![endif]-->
 </head>
 <body id="target">
-	<header>
-		<img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="">
-		<h1><a href="http://localhost/index.php">JavaScript</a></h1>
-	</header>
-	<nav>
-		<ol>
-			<?php
-        while($row = mysqli_fetch_assoc($result)){
-          echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
-        }
-			 ?>
-		</ol>
-	</nav>
-	<div id="control">
-		<input type="button" value="white" id="white_btn"/>
-		<input type="button" value="black" id="black_btn"/>
-    <a href="http://localhost/write.php">쓰기</a>
-	</div>
-	<article>
-		<?php
-      if(empty($_GET['id'])==false){
-        $sql = "SELECT topic.id, title, name, description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_assoc($result);
-        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-        echo '<p>'.htmlspecialchars($row['name']).'</p>';
-        echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
-      }
-		 ?>
-	</article>
+  <div class="container">
+    <header class="jumbotron text-center">
+      <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" class="img-circle" id="logo">
+      <h1><a href="http://localhost/index.php">JavaScript</a></h1>
+    </header>
+    <div class="row">
+      <nav class="col-md-3">
+        <ol class="nav nav-pills nav-stacked">
+          <?php
+            while($row = mysqli_fetch_assoc($result)){
+              echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+            }
+           ?>
+        </ol>
+      </nav>
+      <div class="col-md-9">
+        <article>
+          <?php
+            if(empty($_GET['id'])==false){
+              $sql = "SELECT topic.id, title, name, description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
+              $result = mysqli_query($conn, $sql);
+              $row = mysqli_fetch_assoc($result);
+              echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+              echo '<p>'.htmlspecialchars($row['name']).'</p>';
+              echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
+            }
+           ?>
+        </article>
+        <hr>
+        <div id="control">
+          <div class="btn-group" role="group" arai-label="...">
+            <input type="button" value="white" id="white_btn" class="btn btn-default btn-lg"/>
+            <input type="button" value="black" id="black_btn" class="btn btn-default btn-lg"/>
+          </div>
+          <a href="http://localhost/write.php" class="btn btn-success btn-lg">쓰기</a>
+        </div>
+      </div>
+    </div>
+  </div>
 	<script src="http://localhost/script.js"></script>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
